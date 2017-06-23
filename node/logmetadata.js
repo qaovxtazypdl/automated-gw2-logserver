a2d8a9bd111b84f56cd18091e223828575ef3707
Jonathan Zhao <qaovxtazypdl@hotmail.com>
2017-06-23 15:12:52 +0000

    relocate nginx

--- node/logmetadata.js
+++ node/logmetadata.js
@@ -2,6 +2,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
 	  res.writeHead(200, {'Content-Type': 'text/plain'});
-	  res.end('Hello World node\n');
+	  res.end('Hello World node\nedit 2\n');
 }).listen(8081, 'localhost');
 console.log('Server running at http://localhost:8081/');
