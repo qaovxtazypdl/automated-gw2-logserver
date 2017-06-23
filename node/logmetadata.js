85c07b5f467673b3b764d73020783c54ab11436c
Jonathan Zhao <qaovxtazypdl@hotmail.com>
2017-06-23 04:55:35 +0000

    Hello World!

---
+++ node/logmetadata.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env nodejs
+var http = require('http');
+http.createServer(function (req, res) {
+	  res.writeHead(200, {'Content-Type': 'text/plain'});
+	  res.end('Hello World node\n');
+}).listen(8081, 'localhost');
+console.log('Server running at http://localhost:8081/');
