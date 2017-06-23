728aa8f659c4e85ae03dece37edb47f5bf315ba6
Jonathan Zhao <qaovxtazypdl@hotmail.com>
2017-06-23 19:36:59 +0000

    simple frontend request

--- js/src/index.jsx
+++ js/src/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import {render} from 'react-dom';
+import request from 'browser-request';
+
+const API_URL = 'https://logs.xn--jonathan.com/api/logmetadata';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {response: false};
+  }
+
+  componentWillMount() {
+    request(API_URL, (er, res) => {
+      if (er) throw er;
+      this.setState({response: JSON.parse(res.response)});
+    });
+  }
   render () {
-    const lol = "blach blah";
+    if (!this.state.response) return null;
     return <div>
       <p>Here are all the logs recorded...</p>
-      {`${lol}`}
+      {JSON.stringify(this.state.response)}
     </div>;
   }
 }
