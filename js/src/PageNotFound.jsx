91909b331e8e12e96f7dc1576d9c45b24ed4aef5
Jonathan Zhao <qaovxtazypdl@hotmail.com>
2017-06-25 23:42:20 +0000

    center page not found logo

--- js/src/PageNotFound.jsx
+++ js/src/PageNotFound.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import css from './__style__/PageNotFound.css';
+
 class PageNotFound extends React.Component {
   render() {
-    return <section>
-      <img src="/img/404.jpg" alt="404" />
+    return <section className="fourohfour-section">
+      <div className="fourohfour-container">
+        <img className="fourohfour-img" src="/img/404.jpg" alt="404" />
+      </div>
     </section>;
   }
 }
